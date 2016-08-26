6b4796f70ba3fe0078176cf8d760ea3c5e876b4a
Russell Osborne <russell@burningpony.com>
2016-08-25 22:26:30 -0400

    Adding Crash Course Code

--- game.rb
+++ game.rb
@@ -3,7 +3,7 @@ class Card
 
   def initialize(suit, value)
     @suit = suit
-    @value = value
+    @value = value.to_s
   end
 
   def value
@@ -11,6 +11,53 @@ class Card
   end
 
   def to_s
-    "#{@value}-#{@suit}"
+    return @suit + "-" + @value
   end
 end
+
+class Deck
+  def initialize
+    @cards = []
+    suits = ["clubs", "hearts", "diamonds", "spades"]
+    numbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    face_cards = ["J", "Q", "K", "A"]
+
+    suits.each do |suit|
+      numbers.each do |value|
+         @cards << Card.new(suit, value)
+      end
+
+      face_cards.each do |value|
+         @cards << Card.new(suit, value)
+      end
+    end
+
+    @cards = @cards.shuffle
+  end
+
+  def cards
+    @cards
+  end
+
+  def to_s
+    string = ""
+
+    @cards.each do |card|
+      string = string + "\n" + card.to_s
+    end
+
+    return string
+  end
+end
+
+deck = Deck.new
+
+puts deck
+
+ten_of_clubs = Card.new("clubs", "10")
+
+two_of_swords = Card.new("swords", 2)
+
+ten_of_clubs_as_string = ten_of_clubs.to_s
+puts ten_of_clubs_as_string
+puts two_of_swords
